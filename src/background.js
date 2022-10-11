e2004096052f5a796e3b51bdb1a51df5c54ae5f3
Simon <simon@marchantweb.com>
2022-10-11 15:56:04 -0400

    RC8 - Allow application to quit on close on macOS.

--- src/background.js
+++ src/background.js
@@ -92,18 +92,8 @@ function createWindow() {
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
-
-app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-})
+        app.quit();
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
